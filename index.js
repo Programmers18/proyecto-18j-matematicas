f2aff020bb6eab680d3fa26e8f2c8a3b613af9f4
Jonny D. Lezcano <jonnyda1811@gmail.com>
2020-08-30 16:51:35 -0400

    Funcionalidad para validar numeros OK

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la suma
      */
     suma: function(n1, n2) {
-        return n1 + n2;
+        return (this.esNumero(n1, n2) ? n1 + n2 : this.mensajeError());
     },
     /**
      * Resta de dos numeros
@@ -17,7 +17,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la resta
      */
     resta: function(n1, n2) {
-        return n1 - n2;
+        return (this.esNumero(n1, n2) ? n1 - n2 : this.mensajeError());
     },
     /**
      * Multiplicacion de dos numeros
@@ -27,7 +27,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la multiplicacion
      */
     multiplicacion: function(n1, n2) {
-        return n1 * n2;
+        return (this.esNumero(n1, n2) ? n1 * n2 : this.mensajeError());
     },
     /**
      * Division de dos numeros
@@ -37,6 +37,24 @@ module.exports = {
      * @param {*} n2 Numero dos de la division
      */
     division: function(n1, n2) {
-        return n1 / n2;
+        return (this.esNumero(n1, n2) ? n1 / n2 : this.mensajeError());
+    },
+    /**
+     * Mensaje de error que se ejecuta cuando pasemos parametros 
+     * que no sean numericos
+     */
+    mensajeError: function() {
+        console.log('Valor a los dos valores no son numericos');
+    },
+    /**
+     * Funcion que comprueba que los parametros son numericos
+     * @param {*} n1 
+     * @param {*} n2 
+     */
+    esNumero: function (n1, n2) {
+        if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+            return false;
+        }
+        return true;
     }
 }
--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,12 @@
 const m = require('.');
 
+console.log(m.suma(1, 2));
+
+console.log(m.suma(1, 2));
+
+console.log(m.suma(true, 2));
+
 console.log(m.suma(2,3));
 console.log(m.resta(2,3));
 console.log(m.multiplicacion(2,3));
-console.log(m.division(2,3));
+console.log(m.division('dwadwa','fdsfdfdsafsdf'));
